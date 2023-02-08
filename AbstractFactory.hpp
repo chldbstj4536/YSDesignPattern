82ff54ff4128fdbbdbd33d8bdd41b516a1cdd9f5
최윤서 <chldbstj4536@gmail.com>
2023-02-08 16:24:05 +0900

    싱클톤 패턴 추가

---
+++ AbstractFactory.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <concepts>
+#include <type_traits>
+#include <tuple>
+
+namespace YS::DesignPattern::Creational
+{
+    namespace
+    {
+        template<typename Element, typename... Set>
+        concept HasElement = (std::same_as<Element, Set> || ...);
+
+        template<typename Factory, typename Product>
+        concept ImplementCreateFunc = requires
+        {
+            {Factory::template Create<Product>()};
+        };
+
+        template<typename Factory, typename... Products>
+        concept ImplementFactory = (ImplementCreateFunc<Factory, Products> && ...);
+    }
+
+    template<typename... AbstractProducts>
+    class AbstractFactory
+    {
+    public:
+        template<typename Factory, typename Product>
+        requires HasElement<Product, AbstractProducts...> && ImplementFactory<Factory, AbstractProducts...>
+        static Product* CreateProduct()
+        {
+            return Factory::template Create<Product>();
+        }
+        template<typename Factory, typename... Products>
+        static std::tuple<Products* ...> CreateProducts()
+        {
+            return (std::make_tuple((CreateProduct<Factory, Products>()) ...));
+        }
+        template<typename Factory>
+        static std::tuple<AbstractProducts* ...> CreateAllProducts()
+        {
+            return (std::make_tuple((CreateProduct<Factory, AbstractProducts>()) ...));
+        }
+    };
+}
