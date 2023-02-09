e70213cb13bb61a30cb8ad2a32e266965ecf8f29
최윤서 <chldbstj4536@gmail.com>
2023-02-09 17:21:48 +0900

    싱클톤 패턴, YSUtility 모듈 추가

---
+++ ysSingleton.hpp
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include <memory>
+#include <iostream>
+#include <ysUtility.hpp>
+
+namespace YS::DesignPattern::Creational
+{
+    template<class T>
+    class Singleton : public PassKey<Singleton<T>>
+    {
+    private:
+        Singleton() { std::cout << "Singleton()\n"; }
+    protected:
+        Singleton(Singleton const &) = delete;
+        Singleton(Singleton &&) = delete;
+        ~Singleton() { std::cout << "~Singleton()\n"; }
+        Singleton& operator=(Singleton const &) = delete;
+        Singleton& operator=(Singleton &&) = delete;
+
+        Singleton(PassKey<Singleton>) { std::cout << "Singleton(PassKey<Singleton>)\n"; }
+
+    public:
+        static std::shared_ptr<T> GetInstance()
+        {
+            static std::shared_ptr<T> pInstance = std::make_shared<T>(Singleton());
+            return pInstance;
+        }
+    };
+}
